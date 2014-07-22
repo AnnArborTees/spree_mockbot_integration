3642a55e558327e4205c2a913dc1cd9a0898b027
Resonious <metreckk@gmail.com>
2014-07-22 13:50:40 -0400

    Authentication is good to go\!

--- spec/support/endpoint_actions.rb
+++ spec/support/endpoint_actions.rb
@@ -9,8 +9,9 @@ class EndpointActions
       idea_stub = Endpoint::Stub.create_for Spree::Mockbot::Idea
       expected_email = options[:email]
       expected_token = options[:token]
-      # config.before context is ducked up and our defined methods aren't available.
-      authenticate = method(:authenticate)
+      # config.before messes up the context, so we encapsulate our authenticate
+      # method here.
+      auth = method :authenticate
 
       config.before :suite do
         # Overwrite default index to add pagination headers, 
@@ -19,30 +20,31 @@ class EndpointActions
           query = request.uri.query_values
           records = stub.records.dup
 
-          authenticate.call(request, expected_email, expected_token) do
-            # Sorta simulate SQL 'LIKE' predicate.
-            records.select! { |r| 
-              ['sku', 'working_name', 'product_name'].any? { |n| 
-                r[n] && r[n].downcase.include?(query['search'].downcase) } } if query && query['search']
+          # Sorta simulate SQL 'LIKE' predicate.
+          records.select! { |r| 
+            ['sku', 'working_name', 'product_name'].any? { |n| 
+              r[n] && r[n].downcase.include?(query['search'].downcase) } } if query && query['search']
 
-            {
-              body: records.sort_by { |r| r[:sku] },
-              headers: {
-                'Pagination-Limit' => 100,
-                'Pagination-Offset' => 0,
-                'Pagination-TotalCount' => stub.records.compact.count
-              }
+          {
+            body: records.sort_by { |r| r[:sku] },
+            headers: {
+              'Pagination-Limit' => 100,
+              'Pagination-Offset' => 0,
+              'Pagination-TotalCount' => stub.records.compact.count
             }
-          end
+          }
         end
 
         # Overwrite default find to find by sku instead of id.
         idea_stub.mock_response :get, '/:id.json' do |request, params, stub|
-          authenticate.call(request, expected_email, expected_token) do
-            { body: stub.records.find { |r| r and r[:sku] == params[:id] } }
-          end
+          { body: stub.records.find { |r| r and r[:sku] == params[:id] } }
         end
-      end # before :suite
+
+        # And add authentication
+        idea_stub.override_all do |request, params, stub, &supre|
+          auth.call request, expected_email, expected_token, &supre
+        end
+      end # config.before :suite
     end
 
     private
@@ -50,8 +52,6 @@ class EndpointActions
       if @do_authentication
         token = request.headers['Mockbot-User-Token']
         email = request.headers['Mockbot-User-Email']
-        puts "sent token: #{token} sent email: #{email}"
-        puts "expected token: #{expected_token} expected email: #{expected_email}"
         if token && email and token == expected_token && email == expected_email
           yield
         else
